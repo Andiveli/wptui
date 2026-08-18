65e0806fde7090d43c408ab4704fd13c908a8d57
andiveli <andyvt0304@gmail.com>
2026-08-17 20:37:39 -0500

    refactor(bridge): move EventType protocol ownership

--- whatsrust/lib/main.go
+++ whatsrust/lib/main.go
@@ -9,18 +9,6 @@ const (
 )
 
 const (
-	EventTypeSyncProgress         = 0
-	EventTypeAppStateSyncComplete = 1
-	EventTypeReceipt              = 2
-	EventTypeReaction             = 3
-	// Event type 4 is reserved for the removed multiplexed Presence event.
-	EventTypeConnected     = 5
-	EventTypeMessageAction = 6
-	EventTypeChat          = 7
-	EventTypeLogoutResult  = 8
-)
-
-const (
 	MessageTypeText = iota
 	MessageTypeFile
 )
